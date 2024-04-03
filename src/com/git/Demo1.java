37d22fcfa47f09cfe5429086453a1bc5ceadd550
sharathgitha627 <sharathgitha1627@gmail.com>
2024-04-03 12:21:43 +0530

    if block is added and condition is added

--- src/com/git/Demo1.java
+++ src/com/git/Demo1.java
@@ -6,6 +6,15 @@ public class Demo1 {
 	public void add(String data)
 	{
 		System.out.println("data is "+data);
+		
+		if(data.equals("sharath"))
+		{
+			System.out.println("data is valid ");
+		}
+		else
+		{
+			System.out.println("data is not added");
+		}
 	}
 	
 	public static void main(String[] args) {
