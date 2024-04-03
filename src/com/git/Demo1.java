2c762d8ad9c64ca0d10140f67570bdfb91a6255d
sharathgitha627 <sharathgitha1627@gmail.com>
2024-04-03 12:19:47 +0530

    all files added to local repo

---
+++ src/com/git/Demo1.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class Demo1 {
+	
+	
+	public void add(String data)
+	{
+		System.out.println("data is "+data);
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is first statement");
+	}
+
+}
